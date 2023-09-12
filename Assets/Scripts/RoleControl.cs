1e0785a6cbf1b90e2cab87ef73738f3b21b16fe5
yya404 <4a9k0031@stust.edu.tw>
2023-09-12 18:33:44 +0800

    人物名稱移動(還不能正常移動)

--- Assets/Scripts/RoleControl.cs
+++ Assets/Scripts/RoleControl.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class RoleControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5f; // 控制角色移動速度
+
+    private void Update()
     {
-        
+        HandleTouchInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleTouchInput()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // 在此處理觸摸開始的操作
+                    break;
+
+                case TouchPhase.Moved:
+                    // 在此處理觸摸移動的操作
+                    Vector2 touchDeltaPosition = touch.deltaPosition;
+                    Vector3 movement = new Vector3(touchDeltaPosition.x, 0f, touchDeltaPosition.y) * moveSpeed * Time.deltaTime;
+                    transform.Translate(movement);
+                    break;
+
+                case TouchPhase.Ended:
+                    // 在此處理觸摸結束的操作
+                    break;
+            }
+        }
     }
 }
