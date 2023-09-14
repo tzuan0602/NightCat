f20b914cad312441cc65f86c0f82598011c58bbd
tzuan0602 <4a9k0033@stust.edu.tw>
2023-09-14 15:37:43 +0800

    鍵盤先代替滑鼠操作

---
+++ Assets/Scripts/RoleControlKB.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleControlKB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 偵測滑鼠位置&移動
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) // 目前先以鍵盤操作代替
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) 
+        {
+
+        }
+    }
+}
