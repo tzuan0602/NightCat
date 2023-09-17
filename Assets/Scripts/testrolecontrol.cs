fb18cdfe932ac6931cc45f191e4da70d5634fb01
yya404 <4a9k0031@stust.edu.tw>
2023-09-17 21:05:19 +0800

    鍵盤移動test

---
+++ Assets/Scripts/testrolecontrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testrolecontrol : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 移动速度
+
+    private void Update()
+    {
+        HandleKeyboardInput();
+    }
+
+    private void HandleKeyboardInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized * moveSpeed * Time.deltaTime;
+
+        // 应用移动
+        transform.Translate(moveDirection);
+    }
+}
